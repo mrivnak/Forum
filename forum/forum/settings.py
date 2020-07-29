bf842ae95962554695d5d9f92ab6ebe7745c3ea4
Michael Rivnak <mrivnak86@gmail.com>
2020-07-29 05:25:10 -0400

    removed livereload

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forumapp',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'node_modules'),
-)
