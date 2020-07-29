40453aa3347c8ed8e891e79eb1cc431d8710002b
Michael Rivnak <mrivnak86@gmail.com>
2020-07-29 03:18:19 -0400

    Revert "added livereload, removed node_modules"
    This reverts commit d80eb1c31148843198f8b20f053b5dda581b9d5b.

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forumapp',
-    'rest_framework',
-    'livereload',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'forum.urls'
@@ -124,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'node_modules'),
+)
